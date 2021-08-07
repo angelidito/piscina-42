59ceb74db35f06063a02e8ed7b8fd4f1dd9ab5ce
Angel Martinez diez <angmarti@c3r9s5.42madrid.com>
2021-08-07 14:34:13 +0200

    Update from 42

---
+++ *shell00/repo/ex06/git_ignore.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git ls-files . -iou --exclude-standard
+
+#--exclude-per-directory=.gitignore
---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/06 13:48:33 by angmarti          #+#    #+#             */
+/*   Updated: 2021/08/07 14:28:49 by angmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	int	ascii_code;
+
+	ascii_code = 'a';
+	while (ascii_code <= 'z')
+	{
+		write(1, &ascii_code, 4);
+		ascii_code++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
