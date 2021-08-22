872f72da6fabbc9b34c8ce9eef0737c8ea22c188
Angel M. D <angelidito@gmail.com>
2021-08-22 16:02:21 +0200

    Cambios atoi

--- ^rush02/atoi.c
+++ ^rush02/atoi.c
@@ -6,11 +6,16 @@ int	is_permitted_char(char c)
 	return (c >= '0' && c <= '9');
 }
 
-void	atoi_logic(char *str, int *status, unsigned int *num, int *mult)
+int	is_whitespace(char c)
 {
-	if (*status == 0 && *str == ' ')
+	return (c == 32 || (c <= 13 && c >= 9));
+}
+
+void	atoi_logic(char *str, int *status, unsigned int *num)
+{
+	if (*status == 0 && is_whitespace(*str))
 		;
-	else if (*status == 2 && *str == ' ')
+	else if (*status == 1 && is_whitespace(*str))
 		*status = -1;
 	else if (!is_permitted_char(*str))
 		*status = -1;
@@ -22,32 +27,33 @@ void	atoi_logic(char *str, int *status, unsigned int *num, int *mult)
 		else if (*str >= '0' && *str <= '9')
 		{
 			*num = (*num + *str - '0') * 10;
-			*status = 2;
 		}
 	}
+	else if (*str == '\0')
+		*status = 2;
 }
 
 unsigned int	ft_atoi(char *str)
 {
-	int					multiplier;
 	int					status;
 	unsigned int		num;
 
-	multiplier = 1;
 	status = 0;
 	num = 0;
-	while (status != -1 && *str)
+	while (status != -1 && status != 2 && *str)
 	{
-		atoi_logic(str, &status, &num, &multiplier);
+		atoi_logic(str, &status, &num);
 		str++;
 	}
+	if (status == -1)
+		return (0);
 	num /= 10;
-	return (num * multiplier);
+	return (num);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
-		printf("%d", atoi(argv[1]));
+		printf("%d\n", atoi(argv[1]));
 	return (0);
 }
