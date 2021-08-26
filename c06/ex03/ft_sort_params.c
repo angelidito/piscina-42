93b4e9ff940fc61c03a4596ace35c326eb6c1d1d
Angel Martinez diez <angmarti@c3r6s6.42madrid.com>
2021-08-26 19:49:10 +0200

    c06

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/25 17:24:50 by angmarti          #+#    #+#             */
+/*   Updated: 2021/08/25 19:54:19 by angmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] - s2[i] != 0)
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putargs(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+			write (1, &argv[i][j++], 1);
+		write (1, "\n", 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*aux;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				aux = argv[i];
+				argv[i] = argv[j];
+				argv[j] = aux;
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_putargs(argc, argv);
+}
